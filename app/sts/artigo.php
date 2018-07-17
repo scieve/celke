13b776b67e716d57b4b18c334dece4d70ebc5bbf
Ivanildo Pereira <scieve@scieve.com>
2018-07-17 03:44:22 -0300

    122 - URL amigável e nome do artigo no endereço

--- app/sts/artigo.php
+++ app/sts/artigo.php
@@ -1,3 +1,8 @@
+<?php
+if(!isset($seguranca)){
+    exit;
+}
+?>
 <head>
     <meta charset="UTF-8">
     <title></title>
--- index.php
+++ index.php
@@ -1,15 +1,22 @@
 <?php
-
-//Biblioteca auxiliares
-include 'config/config.php';
+session_start();
+ob_start();
 
 $seguranca = true;
+
+//Biblioteca auxiliares
+include_once 'config/config.php';
+include_once 'lib/lib_valida.php';
+
 $url = filter_input(INPUT_GET, 'url', FILTER_SANITIZE_STRING);
+
+$url_limpa = limparurl($url);
+$endereco = explode('/', $url_limpa);
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
     <?php
-    $file = 'app/sts/'.$url.'.php';
+    $file = 'app/sts/'.$endereco[0].'.php';
     if(file_exists($file)){
         include $file;
     }else{
